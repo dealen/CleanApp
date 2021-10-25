3e56f9d408bd6471785f625fa7d1b9a6893b24f7
dealen <jakub.marcickiewicz@outlook.com>
2021-10-25 22:33:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cleanapp/StartActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cleanapp
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity() : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
