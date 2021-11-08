2b67ff465473a379c3dea75436c067a3b616dc25
dealen <jakub.marcickiewicz@outlook.com>
2021-11-08 20:32:11 +0100

    Some work with fragments.

--- app/src/main/java/com/example/cleanapp/StartActivity.kt
+++ app/src/main/java/com/example/cleanapp/StartActivity.kt
@@ -1,13 +1,30 @@
 package com.example.cleanapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cleanapp.databinding.ActivityStartBinding
 
-class StartActivity() : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+    private lateinit var btnGoToSecondPage: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityStartBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_start)
+        val t = Toast.makeText(applicationContext, "Hello! 111", Toast.LENGTH_LONG)
+        t.show()
+    }
+
+    fun click(view: View){
+        val t = Toast.makeText(applicationContext, "2222222222222", Toast.LENGTH_LONG)
+        t.show()
     }
 }
